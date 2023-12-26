6aa6952d363546e72f8cc50c371399e5aa12d260
Waldemar Lehner <mail@waldemarlehner.de>
2023-12-26 22:39:17 +0100

    add sample animations

--- packages/animations/vite.config.ts
+++ packages/animations/vite.config.ts
@@ -1,10 +1,13 @@
-import {defineConfig} from 'vite';
-import motionCanvas from '@motion-canvas/vite-plugin';
-import ffmpeg from '@motion-canvas/ffmpeg';
+import { defineConfig } from "vite";
+import motionCanvas from "@motion-canvas/vite-plugin";
+import ffmpeg from "@motion-canvas/ffmpeg";
+import { globSync } from "glob";
 
 export default defineConfig({
   plugins: [
-    motionCanvas(),
+    motionCanvas({
+      project: globSync("./src/*/*.ts").map((e) => "./" + e),
+    }),
     ffmpeg(),
   ],
 });
